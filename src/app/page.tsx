44c7dbcdf0705f024d63ae79eae8395301eae58a
mouhamad <mouhamadx92x@gmail.com>
2024-01-29 23:13:18 +0100

    create form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Box, Button, Input } from "@mui/joy";
+import React, { useState } from "react";
 
 const ToDo = () => {
-    return <div>page</div>;
+    const [inputValue, setInputValue] = useState("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("Input Value:", inputValue);
+    };
+
+    return (
+        <Box component="form" onSubmit={submitHandler}>
+            <Input
+                placeholder="Try to submit with no text!"
+                value={inputValue}
+                onChange={handleChange}
+                required
+            />
+            <Button type="submit">Submit</Button>
+        </Box>
+    );
 };
 
 export default ToDo;
